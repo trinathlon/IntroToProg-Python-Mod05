88f6fb5f083295cb52c30b3267a1615daa14eb12
trinathlon <159986208+trinathlon@users.noreply.github.com>
2024-02-13 22:07:28 -0800

    Add files via upload
    Uploading assignment five python script and documentation file.

---
+++ Assignment05.py
@@ -0,0 +1,125 @@
+# ------------------------------------------------------------------------------------------ #
+# Title: Assignment05
+# Desc: This assignment demonstrates using dictionaries, files, and exception handling
+# Change Log: (Who, When, What)
+#   NCastek,2/12/2024,Created Script
+# ------------------------------------------------------------------------------------------ #
+
+import json
+
+# Define the Data Constants
+MENU: str = '''
+---- Course Registration Program ----
+  Select from the following menu:  
+    1. Register a Student for a Course.
+    2. Show current data.  
+    3. Save data to a file.
+    4. Exit the program.
+----------------------------------------- 
+'''
+# Define the Data Constants
+FILE_NAME: str = "Enrollments.json"
+
+# Define the Data Variables and constants
+student_first_name: str = ""  # Holds the first name of a student entered by the user.
+student_last_name: str = ""  # Holds the last name of a student entered by the user.
+course_name: str = ""  # Holds the name of a course entered by the user.
+json_data: str = ""  # Holds the json data
+file = None  # Holds a reference to an opened file.
+menu_choice: str = ""  # Hold the choice made by the user.
+student_data: dict = {}  # one row of student data
+students: list = []  # a table of student data
+
+
+# When the program starts, read the file data into a list of lists (table)
+# Extract the data from the file
+try:
+    file = open(FILE_NAME, "r")
+    students = json.load(file)
+except FileNotFoundError as e:
+    print("Enrollments.json file must exist before running this script.\n")
+    print("Built in python error info: ")
+    print(e, e.__doc__, type(e), sep='\n')
+except Exception as e:
+    print("There was a non-specific error!\n")
+    print("-- Technical Error Message --")
+    print(e, e.__doc__, type(e), sep='\n')
+finally:
+    if not file.closed:
+        file.close()
+
+# Present and Process the data
+while (True):
+
+    # Present the menu of choices
+    print(MENU)
+    menu_choice = input("What would you like to do: ")
+
+    # Input user data
+    if menu_choice == "1":  # This will not work if it is an integer!
+        try:
+            student_first_name = input("Enter the student's first name: ")
+            if not student_first_name.isalpha():
+                raise ValueError("Student's first name should only contain alphabetic characters.")
+
+            student_last_name = input("Enter the student's last name: ")
+            if not student_last_name.isalpha():
+                raise ValueError("Student's last name should only contain alphabetic characters.")
+
+            course_name = input("Please enter the name of the course: ")
+            student_data = {"FirstName": student_first_name, "LastName": student_last_name,
+                            "CourseName": course_name}
+            students.append(student_data)
+            print(f"You have registered {student_first_name} {student_last_name} for {course_name}.")
+
+        except ValueError as e:
+            print(e)
+            print("--- Technical Error Message ---")
+            print(e.__doc__)
+            print(e.__str__())
+        except Exception as e:
+            print("There was a non-specific error!\n")
+            print("--- Technical Error Message ---")
+            print(e, e.__doc__, type(e), sep='\n')
+
+        continue
+
+    # Present the current data
+    elif menu_choice == "2":
+
+        # Process the data to create and display a custom message
+        print("-"*50)
+        for student in students:
+            print(f"Student {student["FirstName"]} {student["LastName"]} is enrolled in {student["CourseName"]}")
+        print("-"*50)
+        continue
+
+    # Save the data to a file
+    elif menu_choice == "3":
+        try:
+            file = open(FILE_NAME, "w")
+            json.dump(students, file)
+            file.close()
+            print("The following data was saved to file!")
+            for student in students:
+                print(f"Student {student['FirstName']} {student['LastName']} is enrolled in {student["CourseName"]}")
+        except TypeError as e:
+            print("Please check that the data is a valid JSON format\n")
+            print("-- Technical Error Message --")
+            print(e, e.__doc__, type(e), sep='\n')
+        except Exception as e:
+            print("-- Technical Error Message --")
+            print("Built-In Python Error Info: ")
+            print(e, e.__doc__, type(e), sep='\n')
+        finally:
+            if not file.closed:
+                file.close()
+        continue
+
+    # Stop the loop
+    elif menu_choice == "4":
+        break  # out of the loop
+    else:
+        print("Please only choose option 1, 2, or 3")
+
+print("Program Ended")
